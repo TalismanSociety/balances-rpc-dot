e4228fc2863b662fe1754e9992c602f64989d468
James Sangalli <j.l.sangalli@gmail.com>
2021-08-19 11:50:20 +1000

    refactor AccountInfo.js

--- util/AccountInfo.js
+++ util/AccountInfo.js
@@ -3,26 +3,26 @@ const registry = new types.TypeRegistry();
 const util_crypto = require('@polkadot/util-crypto');
 const keyring = require('@polkadot/keyring');
 const request = require('request');
+const systemHash = "26aa394eea5630e07c48ae0c9558cef7"; //util_crypto.xxhashAsHex("System", 128);
+const accountHash = "b99d880ec681799c0cf30e8886371da9"; //util_crypto.xxhashAsHex("Account", 128);
+const accountTuple = { "AccountInfo": "(u64, u64, u64, u64, u64)" };
+const lastTypeKey = "(u64, u64, u64, u64, u64)";
+registry.register(accountTuple);
 
 module.exports = class AccountInfo {
 
     constructor(address, endpoint) {
         this.endpoint = endpoint;
-        const systemHash = util_crypto.xxhashAsHex("System", 128);
-        const accountHash = util_crypto.xxhashAsHex("Account", 128);
-        const addressBytes = keyring.decodeAddress(address);
-        const hashedAddress = util_crypto.blake2AsHex(addressBytes, 128);
-        this.payload = systemHash.replace("0x", "")
-            + accountHash.replace("0x", "")
-            + hashedAddress.replace("0x", "")
-            + Buffer.from(addressBytes).toString('hex');
+        this.addressBytes = keyring.decodeAddress(address);
+        this.hashedAddress = util_crypto.blake2AsHex(this.addressBytes, 128).replace("0x", "");
+        this.addressHex = Buffer.from(this.addressBytes).toString('hex');
     }
 
     async getAccountInfo() {
-        const accountInfoRaw = await this.getAccountInfoRaw();
-        const rawBytes = JSON.parse(accountInfoRaw.body).result;
+        const payload = "0x" + systemHash + accountHash + this.hashedAddress + this.addressHex;
+        const rawBytes = await this.getStorageStateRaw(payload);
         const decoded = JSON.stringify(this.decodeResult(rawBytes));
-        const decodedArray = decoded.replace("[", "").replace("]", "").split(",");
+        const decodedArray = JSON.parse(decoded);
         return {
             "nonce": decodedArray[0],
             "index": decodedArray[1],
@@ -32,7 +32,7 @@ module.exports = class AccountInfo {
         }
     }
 
-    async getAccountInfoRaw() {
+    async getStorageStateRaw(payload) {
         return new Promise((resolve, reject) => {
             request(this.endpoint, {
                 method: "POST",
@@ -40,20 +40,20 @@ module.exports = class AccountInfo {
                     id: 1,
                     jsonrpc: "2.0",
                     method: "state_getStorage",
-                    params: ["0x" + this.payload]
+                    params: [payload]
                 }),
                 headers: { "Content-Type": "application/json" }
             }, (error, response, body) => {
-                if(error) reject(error);
-                else resolve(response);
+                if(error) {
+                    reject(error);
+                } else {
+                    resolve(JSON.parse(response.body).result);
+                }
             });
         });
     }
 
     decodeResult(rawBytes) {
-        const accountTuple = { "AccountInfo": "(u64, u64, u64, u64, u64)" };
-        const lastTypeKey = "(u64, u64, u64, u64, u64)";
-        registry.register(accountTuple);
         return types.createType(registry, lastTypeKey, rawBytes);
     }
 
